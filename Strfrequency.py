a99bbec75c61c3745b0908b9e242138098c6ab78
POOJITHredd <51952135+POOJITHredd@users.noreply.github.com>
2020-09-13 10:56:24 +0530

    Create Strfrequency.py

---
+++ Strfrequency.py
@@ -0,0 +1,19 @@
+from collections import Counter 
+
+  
+# initializing string  
+
+test_str = "poojith"
+
+  
+# using collections.Counter() to get  
+# count of each element in string  
+
+res = Counter(test_str) 
+
+  
+# printing result  
+
+print ("Count of all characters in GeeksforGeeks is :\n "
+
+                                           +  str(res)) 
